737ac369bed895e38a4b9640192d30e36e2f3fc7
Jorge Santana <jorgersantana27@gmail.com>
2023-10-06 11:34:54 -0400

    main page layout partials - added

---
+++ src/partials/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="main-footer">
+            <strong>Jorge Ricardo Santana Lora - A00109060</strong>
+            <div className="float-right d-none d-sm-inline-block">
+                AdminLTE.io Template <b className="ml-3">Version</b> 3.2.0
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/partials/SideBar.js
@@ -0,0 +1,99 @@
+import React from 'react';
+
+const SideBar = () => {
+    return (
+        <aside className="main-sidebar sidebar-dark-primary elevation-4">
+            <a href="/" className="brand-link">
+                <img src="dist/img/ReactLogo.png" alt="Logo" className="brand-image img-circle elevation-3" style={{ opacity: ".8" }} />
+                <span className="brand-text font-weight-light">Finanzas Personales</span>
+            </a>
+            <div className="sidebar">
+                <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div className="image">
+                        <img src="dist/img/user8-128x128.jpg" className="img-circle elevation-2" alt="User" />
+                    </div>
+                    <div className="info">
+                        <a href="/" className="d-block">Jorge Santana</a>
+                    </div>
+                </div>
+
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link active">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>
+                                    Dashboard
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">
+                                <i class="nav-icon fas fa-wallet"></i>
+                                <p>
+                                    Budget
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon far fa-envelope"></i>
+                                <p>
+                                    CRUD's
+                                    <i class="fas fa-angle-left right"></i>
+                                    <span class="badge badge-info right">7</span>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="/" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Tipos Egresos</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Tipos Ingresos</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Clasificaciones Egresos</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Tipos Pago</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Egresos</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Ingresos</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Usuarios</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+    )
+}
+
+export default SideBar;
