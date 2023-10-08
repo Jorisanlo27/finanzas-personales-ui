f448c1b8f69b804d726b74728aacafc3178d677b
Jorge Santana <jorgersantana27@gmail.com>
2023-10-08 16:21:28 -0400

    react router - implemented in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
+import TiposPago from './pages/TiposPago';
 import Footer from './partials/Footer';
 import Header from './partials/Header';
 import SideBar from './partials/SideBar';
@@ -9,7 +11,15 @@ function App() {
     <div className="wrapper">
       <Header />
       <SideBar />
-      <Dashboard />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Dashboard />} />
+            <Route path="/tiposPago" element={<TiposPago />} />
+            <Route path="*" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
