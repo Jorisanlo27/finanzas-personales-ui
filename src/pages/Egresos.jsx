b5d2c399515a28b3a58f6e8e2def68c5aa01264c
Jorge Santana <jorgersantana27@gmail.com>
2023-10-09 22:48:58 -0400

    egresos page - added

---
+++ src/pages/Egresos.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import api from "../api/axiosConfig";
+import EgresosCrud from "../components/Egresos/Crud";
+
+function Egresos() {
+    const [tiposEgresos, setTiposEgresos] = useState([]);
+    const [clasificacionEgresos, setClasificacionEgresos] = useState([]);
+    const [tiposPago, setTiposPago] = useState([]);
+    const [egresos, setEgresos] = useState([]);
+
+    useEffect(() => {
+        (async () => await load())();
+    }, []);
+
+    async function load() {
+        const result1 = await api.get("/egresos/");
+        setEgresos(result1.data);
+
+        const result2 = await api.get("/tiposEgresos/");
+        setTiposEgresos(result2.data);
+
+        const result3 = await api.get("/clasificacionEgresos/");
+        setClasificacionEgresos(result3.data);
+
+        const result4 = await api.get("/tiposPago/");
+        setTiposPago(result4.data);
+    }
+
+    return (
+        <div className="content-wrapper mt-3" style={{ minHeight: "86vh" }}>
+            <h1 className="text-center">CRUD - Egresos</h1>
+            <EgresosCrud load={load} tipos={tiposEgresos} clasificaciones={clasificacionEgresos} tiposPago={tiposPago} egresos={egresos} />
+        </div>
+    )
+}
+
+export default Egresos;
